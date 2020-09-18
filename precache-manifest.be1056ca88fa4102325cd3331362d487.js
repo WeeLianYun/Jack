a7ab96a21d0093577aa784242d5a3012412056c4
WeeLianYun <weelianyun@hotmail.com>
2020-09-18 17:06:40 +0800

    Updates

---
+++ precache-manifest.be1056ca88fa4102325cd3331362d487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49041d824b07155c94952733edb3ba9e",
+    "url": "/Jack/index.html"
+  },
+  {
+    "revision": "ed1bd8dd4ddf7cc60114",
+    "url": "/Jack/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7abe38b47505c42d57d2",
+    "url": "/Jack/static/js/2.c54a9235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jack/static/js/2.c54a9235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1bd8dd4ddf7cc60114",
+    "url": "/Jack/static/js/main.8fec150c.chunk.js"
+  },
+  {
+    "revision": "cb9881aedac780ad7b4c",
+    "url": "/Jack/static/js/runtime-main.2fdca59a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jack/static/media/logo.5d5d9eef.svg"
+  }
+]);
